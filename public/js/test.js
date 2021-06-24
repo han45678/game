35ee61a5db2619ab42ee65ebb1ed38c808d1a492
han45678 <han0910812144@gmail.com>
2021-06-24 17:01:39 +0800

    Add files via upload

---
+++ public/js/test.js
@@ -0,0 +1,15 @@
+const name = 'HAN'
+const img = "/images/pexels-remy-mcdonald-3054292.jpg"
+
+
+function test() {
+    const text = "Hello World"
+    alert(text)
+}
+
+//這邊一定要加上去 不然沒有用
+export {
+    test,
+    name,
+    img
+}
